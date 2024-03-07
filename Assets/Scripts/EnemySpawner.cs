b6b8473ce28432826dc3bf59cae8014cca524995
AxelVanAvondt <axel.vanavondt@student.arcadiascholen.be>
2024-03-07 11:13:50 +0100

    Enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public static EnemySpawner instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    public List<GameObject> Path1;
+    public List<GameObject> Path2;
+    public List<GameObject> Enemies;
+
+    private void SpawnEnemy(int type, Path path)
+    {
+        var newEnemy = Instantiate(Enemies[type], Path1[0].transform.position, Path1[0].transform.rotation);
+        var script = newEnemy.GetComponentInParent<Enemy>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
